bdf5918a52c30ae641e4e11a9dde66f0999c85d3
TenzinRenouard <kzhw2082@leeds.ac.uk>
2025-12-02 17:03:51 +0000

    added macros for coloured output text

---
+++ colours.h
@@ -0,0 +1,16 @@
+#ifndef COLOURS_H
+#define COLOURS_H
+
+
+#define RED "\x1b[31m"
+#define BLUE "\x1b[94m"
+#define YELLOW "\x1b[93m"
+#define GREEN "\x1b[92m"
+#define CYAN "\x1b[96m"
+#define MAGENTA "\x1b[95m"
+
+
+#define RESET "\x1b[0m"
+
+
+#endif /*COLOURS_H*/
