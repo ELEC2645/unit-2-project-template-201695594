efff7e6c5f30147bbfe95e5f9bf06b207b1ebcb9
TenzinRenouard <kzhw2082@leeds.ac.uk>
2025-12-05 12:47:33 +0000

    test file for checking responses of individual functions

---
+++ test.c
@@ -0,0 +1,35 @@
+/*TESTING FILE*/
+#include "funcs.h"
+#include "menu_funcs.h"
+#include "colours.h"
+
+#include "stdio.h"
+
+
+converter test = {
+    .name = "TestConverter",
+    .type = 2,
+    .V_o = 50,
+    .V_i = 20,
+    .I_o = -1,
+    .I_i = -1,
+    .F_s = -1,
+    .R_l = 300,
+    .i_rip = -2,
+    .v_rip = -1,
+    .i_rip2 = -2,
+    .v_rip2 = -2,
+    .k = -1,
+    .C_n = -2,
+    .C_o = -1,
+    .L = -1,
+    .L2 = -2,
+};
+
+void main(void){
+    printf("Current test converter:\n");
+    print_converter(&test);
+    printf("Computing as boost converter\n");
+    menu_item_4(&test);
+    print_converter(&test);
+}
