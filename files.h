41a60ee7c30e058f4ae588fdcc7e6bb91c5197a8
TenzinRenouard <kzhw2082@leeds.ac.uk>
2025-12-09 16:48:27 +0000

    added comments

--- files.h
+++ files.h
@@ -1,9 +1,12 @@
 #ifndef FILES_H
 #define FILES_H
 
+// reads .json file at path and puts it into target converter struct
 int read_converter(char *path, converter *target);
+// writes passed converter into .json file at specified path
 int write_converter(char *path, converter *data);
 
+// prints file names currently in ./saves
 void print_saves();
 
 #endif /*FILES_H*/
