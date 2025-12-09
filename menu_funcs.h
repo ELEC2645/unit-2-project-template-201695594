5e56b7c3a0249dc4d30ad99eebb5cd5e8c80b868
TenzinRenouard <kzhw2082@leeds.ac.uk>
2025-12-09 16:48:38 +0000

    comment change

--- menu_funcs.h
+++ menu_funcs.h
@@ -5,7 +5,7 @@ void menu_item_1(converter *active); // set converter name & type
 void menu_item_2(converter *active); // set initial converter parameters
 void menu_item_3(converter *active); // edit individual converter parameters
 void menu_item_4(converter *active); // compute converter
-void menu_item_5(converter *active); // save/export converter design
+void menu_item_5(converter *active); // load/save/export converter design
 
 void print_converter(converter *active, int flag); // print a numbered list of all the parsed converter struct fields
 
